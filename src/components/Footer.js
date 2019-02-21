66ccf785d46b29a32fd973d66c3218b4a4c94376
Rakesh Tembhurne <tj2point0@gmail.com>
2019-02-21 15:53:01 +0530

    Initial  commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
                     <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
-                    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                    <li>&copy; Untitled</li><li>Design: HTML5 UP</li>
                 </ul>
             </section>
         )
