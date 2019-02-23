459d9a7d3da284b002b5d14c3914be4ff56f038b
Rakesh Tembhurne <tj2point0@gmail.com>
2019-02-23 18:10:06 +0530

    Added google map and ui changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,12 +91,16 @@ class Homepage extends React.Component {
                         <header className="major">
                             <h2>Contact Us</h2>
                         </header>
-                        <ul className="copyright ul">
-                            <li><strong>Name:</strong> <p>Rakesh Tembhurne</p></li>
-                            <li><strong>Email:</strong> <p>rakesh@zeromileedutech.com</p></li>
-                            <li><strong>Phone:</strong> <p>7020138038</p></li>
-                            <li className="specialLi"><strong>Address:</strong> <p>Flat No. 202, Sukhkarta Apartment, Suyog Nagar, Near Narendra Nagar Fire Station, Ring Road, Nagpur, 440015</p></li>
-                        </ul>
+                    </div>
+                    <ul className="copyright ul">
+                        <li><strong>Name</strong> <p>Rakesh Tembhurne</p></li>
+                        <li><strong>Email</strong> <p>rakesh@zeromileedutech.com</p></li>
+                        <li><strong>Phone</strong> <p>+91 7020138038</p></li>
+                        <li className="specialLi"><strong>Address</strong> <p>Flat No. 202, Sukhkarta Apartment, Suyog Nagar, Near Narendra Nagar Fire Station, Ring Road, Nagpur, 440015</p></li>
+                    </ul>
+                    <div class="gmap_canvas">
+                        <iframe width="30%" height="80%" id="gmap_canvas" src="https://maps.google.com/maps?q=Zero%20Mile%20Edutech&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        </iframe>
                     </div>
                 </section>
             </Layout>
